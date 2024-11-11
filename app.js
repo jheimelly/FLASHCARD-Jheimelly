0e2dd178e36e25827db20580c6c7b685beed0704
jheimelly <167384761+jheimelly@users.noreply.github.com>
2024-11-11 11:17:07 -0300

    ok

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 function criacartao(categoria,pergunta,respodta)}
+console.long(categoria,pergunta,respota)
+let container =document.getElementById(`container``)
+let cartao= document.createELement(`article`)
+cartao claNOME
+`)
