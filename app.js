13b6899f9aa7608bea1aee87de304da6d59efc7d
jheimelly <167384761+jheimelly@users.noreply.github.com>
2024-11-12 09:22:53 -0300

    ok

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-function criacartao(categoria,pergunta,respodta)}
-console.long(categoria,pergunta,respota)
-const newLocal = `)`
-let container =document.getElementById(`container``)
-let cartao= document.createELement(`article`)
-cartao claNOME =`cartao`
 
-cartao.innerHTML=
-<div class`=cartao-conteudo>
-<h3programacao</h3>
-<div class=`cartao_conteudo_resposta``>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let respostaEstaVisivel=false
+
+function viracartao()}
+   respostaESTAVISIVEL=!respostaESTAVEL 
+     cartao class.list.toggle('artctive')
