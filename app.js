d2d634fb11c084f9966bec640d9e26f1c65a85e9
jheimelly <167384761+jheimelly@users.noreply.github.com>
2024-11-12 09:01:29 -0300

    ok

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function criacartao(categoria,pergunta,respodta)}
 console.long(categoria,pergunta,respota)
+const newLocal = `)`
 let container =document.getElementById(`container``)
 let cartao= document.createELement(`article`)
 cartao claNOME =`cartao`
 
 cartao.innerHTML=
-<div class`
-`)
+<div class`=cartao-conteudo>
+<h3programacao</h3>
+<div class=`cartao_conteudo_resposta``>
