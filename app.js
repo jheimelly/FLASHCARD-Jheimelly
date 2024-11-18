388f5e3565e3586b9b7b45276998b2c5b5bda308
jheimelly <167384761+jheimelly@users.noreply.github.com>
2024-11-18 11:32:13 -0300

    ok

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ function criaCartao (categoria , perguntas ,resposta) {
                <div class="cartao__conteudo__resposta">
                    <p>$(resposta)</p>
                </div>
-           </div>
+           </div>`
    //iner.HTML insere texto no HTML via JAVASCRIPT
    let respostaEstaVisivel= false
 }
+
+  function vira cartao() {
+    respodtaEStavel=! respostaEstaVisivel
+    cartao.classslist. toglle ('active',respostaEstaVisivel)
+  }
+  //criacao da função viaCartao
+  //ao virar o cartao resposta invisivel ficará invisivel por causa do !
+  //ao clicar no cartao ele adiciona o métado active p/ativar o clic na resposta
+  cartao.addEventListenner ('click', viraCartao)
+  container.appendChild(cartao)
+  //adiciona um evento de escuta para o clic do cartao
+  //ao clicar cirar um item "filho (appendChild)"no cartao
