81f2c020be3556bee355bb7c441da5845ebed1fe
jheimelly <167384761+jheimelly@users.noreply.github.com>
2024-11-18 11:17:57 -0300

    ok

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let respostaEstaVisivel=false
-
-function viracartao()}
-   respostaESTAVISIVEL=!respostaESTAVEL 
-     cartao class.list.toggle('artctive')
+function criaCartao (categoria , perguntas ,resposta) {
+  let container= Document.getElementById('container')
+  //VARIAVEL CONTAINER BUSCADA PELO ID 'CONTAINER'
+   let cartao= document.createElement('article')
+   //VARIAVEL CARTAO QUE CRIA <ARTICLE> NO HTML
+   cartao.className='cartao'
+   //CRIA CLASSE ''cartao para o elemnto criado
+   cartao.innerHTML=`
+   <div class="cartao__conteudo">
+               <h3>$ ( categoria)</h3>
+               <div class="cartao__conteudo__pergunta">
+                   <p>$(pergunta)</p>
+               </div>
+               <div class="cartao__conteudo__resposta">
+                   <p>$(resposta)</p>
+               </div>
+           </div>
+   //iner.HTML insere texto no HTML via JAVASCRIPT
+   let respostaEstaVisivel= false
+}
